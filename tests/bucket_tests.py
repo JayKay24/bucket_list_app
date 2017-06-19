bbe2bf9eab2b01ac66de768c8b37116dc3f8d1d6
James Kinyua <jameskinyua590@gmail.com>
2017-06-19 16:04:19 +0300

    Define Test class BucketistTestResponse

---
+++ tests/bucket_tests.py
@@ -0,0 +1,13 @@
+import unittest
+from bucket_list_app.main import app
+
+class BucketlistTestResponse(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        
+    def test_homepage_works(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        
+if __name__ == '__main__':
+    unittest.main()
