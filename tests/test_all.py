3168c76e585315e02c87077aa0b2b5b9255703ec
James Kinyua <jameskinyua590@gmail.com>
2017-06-19 16:08:46 +0300

    Create test_suite response_suite

---
+++ tests/test_all.py
@@ -0,0 +1,14 @@
+import unittest
+from bucket_tests import BucketlistTestResponse
+
+def suite():
+    """
+    Return a composite testsuite.
+    """
+    
+    response_suite = unittest.makeSuite(BucketlistTestResponse)
+    
+    return unittest.TestSuite((response_suite,))
+    
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
