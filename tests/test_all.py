cd1374338cb10cccb71d6f39f939b0a981fda9ac
James Kinyua <jameskinyua590@gmail.com>
2017-06-20 14:28:57 +0300

    Add test case UserTest to test suite

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,5 +1,6 @@
 import unittest
 from homepage_tests import HomepageTest
+from test_user import UserTest
 
 def suite():
     """
@@ -7,8 +8,9 @@ def suite():
     """
     
     home_suite = unittest.makeSuite(HomepageTest)
+    user_suite = unittest.makeSuite(UserTest)
     
-    return unittest.TestSuite((home_suite,))
+    return unittest.TestSuite((home_suite, user_suite))
     
 if __name__ == '__main__':
     unittest.main(defaultTest='suite')
