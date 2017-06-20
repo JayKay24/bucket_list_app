7a212e42bfa32148b996b3c64395dd6ac92972ee
James Kinyua <jameskinyua590@gmail.com>
2017-06-20 14:21:17 +0300

    Update class User initialize with optional arguments

--- application/classes/user.py
+++ application/classes/user.py
@@ -2,12 +2,13 @@
 This module defines a class to create a User object.
 """
 class User:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name=None, last_name=None, email=None, 
+                 password=None):
         self.user_id = None
         self.fname = first_name
         self.lname = last_name
-        self.email = None
-        self.password = None
+        self.email = email
+        self.password = password
         # Store the user's bucketlists in a list.
         self.bucket_lists = []
         
