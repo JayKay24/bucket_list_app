0b4cf6fcea37c8f95168be1e27f3ed2592de3056
James Kinyua <jameskinyua590@gmail.com>
2017-06-21 10:06:35 +0300

    Remove flask-login configuration and loginmanager object from app.py

--- application/app.py
+++ application/app.py
@@ -3,7 +3,6 @@ This module contains the file application instance.
 """
 
 from flask import Flask, g
-from flask.ext.login import LoginManager, current_user
 
 from config import Configuration # Import the configuration data.
 
@@ -11,10 +10,3 @@ from config import Configuration # Import the configuration data.
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
-login_manager = LoginManager(app)
-login_manager.login_view = "login"
-
-# Create a signal handler to load the current user.
-@app.before_request
-def _before_request():
-    g.user = current_user
