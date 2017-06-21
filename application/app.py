e3a2aa16aaa7566a3ceb7f421bd11f6f4701c08a
James Kinyua <jameskinyua590@gmail.com>
2017-06-21 10:14:43 +0300

    Configure the flask environment setting and silence errors if not set

--- application/app.py
+++ application/app.py
@@ -9,4 +9,7 @@ from config import Configuration # Import the configuration data.
 # Create a flask application instance.
 app = Flask(__name__)
 app.config.from_object(Configuration)
+# Enable flask to import multiple configurations and use the setting
+# defined in the last import.
+app.config.from_envvar('BUCKETLIST_SETTINGS', silent=True)
 
