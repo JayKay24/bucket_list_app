d47d53056f574347c5ddd0b5c972e51f640b98c6
James Kinyua <jameskinyua590@gmail.com>
2017-06-21 10:56:29 +0300

    Create registration template register.html

--- application/classes/user.py
+++ application/classes/user.py
@@ -17,20 +17,3 @@ class User:
         Return the user's full name.
         """
         return self.fname + self.lname
-        
-    # Flask-Login interface.
-    def get_id(self):
-        """
-        Determine ID of user to be stored in the session.
-        """
-        return str(self.id)
-        
-    # Allow users to log in only if the active attribute is set to True.
-    def is_authenticated(self):
-        return True
-        
-    def is_active(self):
-        return self.active
-        
-    def is_anonymous(self):
-        return False
