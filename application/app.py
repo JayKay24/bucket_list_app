6ff2232877f1a253b6d4aa103142fb41d1da9198
James Kinyua <jameskinyua590@gmail.com>
2017-06-21 08:09:29 +0300

    Add a signal handler to load the current user

--- application/app.py
+++ application/app.py
@@ -2,10 +2,19 @@
 This module contains the file application instance.
 """
 
-from flask import Flask
+from flask import Flask, g
+from flask.ext.login import LoginManager, current_user
 
 from config import Configuration # Import the configuration data.
 
 # Create a flask application instance.
 app = Flask(__name__)
 app.config.from_object(Configuration)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
+# Create a signal handler to load the current user.
+@app.before_request
+def _before_request():
+    g.user = current_user
