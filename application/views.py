badb28211a0e1d8f673877738dff4f0bb192b031
James Kinyua <jameskinyua590@gmail.com>
2017-06-21 21:53:48 +0300

    Create test class LoginTest

--- application/views.py
+++ application/views.py
@@ -2,7 +2,7 @@
 This module defines the routes to be used by the flask application instance.
 """
 from flask import render_template, redirect, request, url_for, flash
-from forms import RegistrationForm
+from forms import RegistrationForm, LoginForm
 from app import app
 from models.user import User
 from data_store import users
@@ -22,17 +22,18 @@ def register():
     if request.method == 'POST':
         form = RegistrationForm(request.form)
         if form.validate():
-            first_name = form.first_name.data
+            first_name = str(form.first_name.data)
             last_name = form.last_name.data
             email = form.email.data
             password = form.email.data
             user = User(first_name, last_name, email, password)
+            print(user.fname, user.lname)
             users.append(user)
             flash('User created successfully!', 'success')
             return redirect(url_for('login'))
     else:
         form = RegistrationForm()
-        return render_template('register.html', form=form)
+    return render_template('register.html', form=form)
         
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -40,16 +41,18 @@ def login():
     Return and render login.html template.
     """
     if request.method == 'POST':
-        form = RegistrationForm(request.form)
+        form = LoginForm(request.form)
         if form.validate():
             email = form.email.data
             password = form.password.data
             for user in users:
                 if user.email == email and user.password == password:
+                    flash('You have been successfully logged in!', 'success')
                     return redirect(url_for('homepage'))
                     break
             else:
+                flash('Please register with the application first', 'success')
                 return redirect(url_for('register'))
     else:
         form = RegistrationForm()
-        return render_template('login.html')
+    return render_template('login.html', form=form)
---
+++ tests/test_login.py
@@ -0,0 +1,40 @@
+"""
+This module defines a class LoginTest to test the login view function.
+"""
+import unittest
+from application.main import app
+
+class LoginTest(unittest.TestCase):
+    """
+    Class to instantiate a LoginTest object which tests login/logout
+    functionality.
+    """
+    def setUp(self):
+        self.app = app.test_client()
+        
+    def login(self, email, password):
+        """
+        Helper method to log in the user.
+        """
+        return self.app.post('login', data=dict(
+        email=email, password=password), follow_redirects=True)
+        
+    def logout(self):
+        """
+        Helper method to log out the user.
+        """
+        return self.app.get('/logout', follow_redirects=True)
+        
+    def test_login(self):
+        """
+        Assert login message is in homepage.
+        """
+        response = self.login('jameskinyua@gmail.com', 'admin')
+        assert 'You have been successfully logged in!' in response.data
+        
+    def test_login_invalid_credentials(self):
+        """
+        Assert login returns registration message.
+        """
+        response = self.login('jimmy@gmail.com', 'admin')
+        assert 'Please register with the application first' in response.data
