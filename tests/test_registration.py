e3255d8552fc01dd2b11b01819ceadccdf871485
James Kinyua <jameskinyua590@gmail.com>
2017-06-22 08:07:08 +0300

    Rename response.data to response.status_code in RegistrationTest

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -12,12 +12,12 @@ class RegistrationTest(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
     
-    def test_register_page_returns_200(self):
+    def test_registration_view_returns_200_status_code(self):
         """
         Return a status code of 200.
         """
         response = self.app.get('/register')
-        self.assertEqual(response.data, 200, 
+        self.assertEqual(response.status_code, 200, 
         "a status code of 200 should be sent as a response code")
         
     def test_register_renders_template(self):
