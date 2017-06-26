248833ce249b5562a53a428ec4d8bb0237fe1bc1
James Kinyua <jameskinyua590@gmail.com>
2017-06-26 13:39:42 +0300

    Define a class BucketListApp to manipulate class models

--- application/bucket_list_app.py
+++ application/bucket_list_app.py
@@ -3,8 +3,24 @@ from models.bucket_list import BucketList
 from models.bucket_list_item import BucketListItem
 from models.user import User
 
-all_users = []
-all_bucketlists = []
+class BucketListApp:
+    """
+    Controller class to manipulate class models.
+    """
+    def __init__(self):
+        self.users = []
+        self.current_user = None
+        self.current_bucketlist = None
+        self.current_bucketlist_item = None
         
+    def create_user(self, fname, lname, email, password):
+        """
+        Create a user upon registration.
+        """
+        for user in self.users:
+            if (user.fname == fname and user.lname == lname):
+                return False
+        user = User(fname, lname, email, password)
+        return True
 
 
