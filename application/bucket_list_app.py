876edaf42ea3b9bc50d75170b70a5b42e472944e
James Kinyua <jameskinyua590@gmail.com>
2017-06-27 18:33:28 +0300

    Define a method edit_bucketlist_item in class BucketListApp

--- application/bucket_list_app.py
+++ application/bucket_list_app.py
@@ -135,4 +135,36 @@ class BucketListApp:
                     return False
         else:
             return None
+            
+    def load_bucketlist_item(self, name, description):
+        """
+        Load the current bucketlist item for editing.
+        """
+        if self.current_user is not None:
+            if self.current_bucketlist is not None:
+                for i in range(len(self.current_bucketlist.bucketlist_items)):
+                    if (self.current_bucketlist.bucketlist_items[i].name == name
+                    and self.current_bucketlist.bucketlist_items[i].description == description):
+                        self.current_bucketlist_item = self.current_bucketlist.bucketlist_items.pop(i)
+                        return True
+                else:
+                    return False
+        else:
+            return None
+            
+    def edit_bucketlist_item(self, name, description):
+        """
+        Edit a bucketlist using the strings name and description.
+        """
+        if self.current_user is not None:
+            if self.current_bucketlist is not None:
+                if self.current_bucketlist_item is not None:
+                    self.current_bucketlist_item.name = name
+                    self.current_bucketlist_item.description = description
+                    self.current_bucketlist.bucketlist_items.append(self.current_bucketlist_item)
+                    return True
+                else:
+                    return False
+        else:
+            return None
 
