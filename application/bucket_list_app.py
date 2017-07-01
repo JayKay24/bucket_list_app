96d2a61b3e06fddcfe3a094c35391b3da3fa5c85
James Kinyua <jameskinyua590@gmail.com>
2017-07-01 18:55:17 +0300

    Remove variables all_users, all_bucketlists from module bucket_list_app

--- application/bucket_list_app.py
+++ application/bucket_list_app.py
@@ -2,8 +2,6 @@ from models.bucket_list import BucketList
 from models.bucket_list_item import BucketListItem
 from models.user import User
 
-all_users = []
-all_bucketlists = []
 
 class BucketListApp:
     """
