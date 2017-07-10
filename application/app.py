22fce19fedede30537e03c484992f4936b2c6955
Jimmy <jameskinyua590@gmail.com>
2017-07-10 14:41:35 +0300

    Remove unused imports in app.py

--- application/app.py
+++ application/app.py
@@ -1,10 +1,6 @@
 """
 This module contains the file application instance.
 """
-from models.user import User
-from models.bucket_list import BucketList
-from models.bucket_list_item import BucketListItem
-
 from flask import Flask, g
 
 from config import Configuration # Import the configuration data.
@@ -15,4 +11,5 @@ app.config.from_object(Configuration)
 # Enable flask to import multiple configurations and use the setting
 # defined in the last import.
 app.config.from_envvar('BUCKETLIST_SETTINGS', silent=True)
+
 
